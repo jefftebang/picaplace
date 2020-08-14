5121831c054c396a399c8cebc772d61e76bebb0d
Jeffersson Tebang <jefftebang0118@gmailgit config --global user.name Jeffersson>
2020-08-14 23:59:48 +0800

    fix some erros

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+// const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
@@ -18,25 +18,29 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://picaplace-aa132.web.app"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
 // Config for development only
-if (process.env.NODE_ENV === "development") {
-  app.use(
-    cors({
-      origin: process.env.CLIENT_URL,
-    })
-  );
+// if (process.env.NODE_ENV === "development") {
+//   app.use(
+//     cors({
+//       origin: process.env.CLIENT_URL,
+//     })
+//   );
 
-  app.use(morgan("dev"));
-}
+//   app.use(morgan("dev"));
+// }
 
 const postsRoutes = require("./routes/PostsRoutes");
 const usersRoutes = require("./routes/UsersRoutes");
